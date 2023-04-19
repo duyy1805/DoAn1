f38751cb7e31fc85706611dd27eb2dac7a7a6163
Luna <hamanhduy1805@gmail.com>
2023-04-19 10:24:34 +0700

    Duy

---
+++ client/src/components/navbar.js
@@ -0,0 +1,43 @@
+import { Link as RouterLink, NavLink } from 'react-router-dom';
+import { AppBar, Avatar, Box, Link, Toolbar } from '@material-ui/core';
+import { Logo } from './logo';
+
+
+import DownloadIcon from '@material-ui/icons/PictureAsPdf';
+
+export const Navbar = () => (
+  <AppBar
+    elevation={0}
+
+    sx={{ backgroundColor: '#1e212a' }}
+  >
+    <Toolbar
+      disableGutters
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        minHeight: 64,
+        justifyContent :'space-between',
+        px: 3,
+        py: 1,
+        mr:5
+      }}
+    >
+      <Box
+        component={RouterLink}
+        to="/"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+       <Logo /></Box> 
+       <Box >
+
+      </Box>
+
+     
+    </Toolbar>
+  </AppBar>
+);
